581de24bb15fe3b209df16c41c43279340326056
kritbovorn <kritbovorn@gmail.com>
2023-01-11 22:21:43 +0700

    Add: package()

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { SafeAreaView, StatusBar, Text ,View} from 'react-native';
+import { SafeAreaView, StatusBar, Text ,TouchableHighlight,View} from 'react-native';
 import { gbs } from "./src/utils/import/import_options";
 
 const App = () => {
@@ -9,7 +9,12 @@ const App = () => {
        <SafeAreaView style={{ flex: 1, backgroundColor: '' }}>
          <StatusBar barStyle="dark-content" backgroundColor={"white"}/>
          <View style={[gbs.center, { flex: 1, backgroundColor: '' }]}>
-           
+            <Text style= {[gbs.head5Tri]}>GAMEON</Text>
+            <TouchableHighlight>
+               <View style={[{height: 90}]}>
+                 <Text style= {[]}>Let's Begin</Text>
+               </View>
+            </TouchableHighlight>
          </View>
        </SafeAreaView>
      </Fragment>
