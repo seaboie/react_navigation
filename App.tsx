6b45defe8f81704266d997c009dbeff70e29ee97
kritbovorn <kritbovorn@gmail.com>
2023-01-12 10:55:35 +0700

    Add: package SVG; Add: Images & SVG

--- App.tsx
+++ App.tsx
@@ -1,23 +1,26 @@
 import React, { Fragment } from "react";
-import { SafeAreaView, StatusBar, Text ,TouchableHighlight,View} from 'react-native';
-import { gbs } from "./src/utils/import/import_options";
+import { SafeAreaView, StatusBar, Text, TouchableHighlight, View } from 'react-native';
+import { ChevronLeftIcon, ChevronRightIcon } from "./src/utils/import/import_example";
+import { gbs, sc } from "./src/utils/import/import_options";
 
 const App = () => {
-  return (  
-     <Fragment>
-       <SafeAreaView style={{ flex: 0, backgroundColor: '' }} />
-       <SafeAreaView style={{ flex: 1, backgroundColor: '' }}>
-         <StatusBar barStyle="dark-content" backgroundColor={"white"}/>
-         <View style={[gbs.center, { flex: 1, backgroundColor: '' }]}>
-            <Text style= {[gbs.head5Tri]}>GAMEON</Text>
-            <TouchableHighlight>
-               <View style={[{height: 90}]}>
-                 <Text style= {[]}>Let's Begin</Text>
-               </View>
-            </TouchableHighlight>
-         </View>
-       </SafeAreaView>
-     </Fragment>
+  return (
+    <Fragment>
+      <SafeAreaView style={{ flex: 0, backgroundColor: '' }} />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '' }}>
+        <StatusBar barStyle="dark-content" backgroundColor={"white"} />
+        <View style={[gbs.center, { flex: 1, backgroundColor: '', paddingHorizontal: sc.padMid }]}>
+          <Text style={[gbs.head5Tri, {color: '#20315f'}]}>GAMEON</Text>
+          <TouchableHighlight underlayColor={"lavender"} onPress={() => { }} style={{width: '100%', backgroundColor: '#AD40AF'}}>
+            <View style={[{ height: sc.buttonBottomBarHeight, flexDirection: "row", alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: sc.padMid }]}>
+              
+              <Text style={[{color: '#fff', fontSize: sc.head1, fontFamily: 'Trirong-MediumItalic'}]}>Let's Begin</Text>
+              <ChevronRightIcon height={"65%"} width={"8%"} stroke={"white"} />
+            </View>
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
+    </Fragment>
   );
 }
 
---
+++ src/utils/import/import_example.tsx
@@ -0,0 +1,4 @@
+import ChevronLeftIcon from "../../assets/svg/examples/chevron_left.svg";
+import ChevronRightIcon from "../../assets/svg/examples/chevron_right.svg";
+
+export { ChevronLeftIcon, ChevronRightIcon };
